ed2411c87f4b1232d1b37a69c1fc7ac4c01961d3
maxymkotko <maxkotko@outlook.com>
2023-05-27 04:01:20 +0300

    Added success and error clors

--- src/shared/colors.ts
+++ src/shared/colors.ts
@@ -1,7 +1,9 @@
-enum colors {
+enum Colors {
+	SUCCESS = '#07BC0C',
+	ERROR = '#E74C3C',
 	MILK = '#EFF0EA',
 	DARK_BLUE_ACTIVE = '#1d4cca',
 	EXTRA_LIGHT_BLUE = '#9BE9DC',
 }
 
-export default colors
+export default Colors
